119e5e61618721c82a5bc9ab2483097a8b5fe72b
MAXlMOSoz <thanaporn1max@gmail.com>
2026-05-20 22:41:25 +0700

    Save 1

---
+++ src/app/contact/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Contract() {
+  return (
+    <div>
+      <h1><center>
+        Contract Page
+      </center></h1>
+    </div>
+  );
+}
---
+++ src/app/service/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Sevice() {
+  return (
+    <div>
+      <h1><center>
+        Sevice Page
+      </center></h1>
+    </div>
+  );
+}
